7b313a534fe7451745b230f9b68fe5e86fba9569
baedormann <89854178+baedormann@users.noreply.github.com>
2022-03-16 10:19:21 +0900

    22-03-16

---
+++ Board/src/main/java/com/kh/board/service/BoardService.java
@@ -0,0 +1,11 @@
+package com.kh.board.service;
+
+import java.util.List;
+
+import com.kh.board.vo.BoardVO;
+
+public interface BoardService {
+	//게시글 목록 조회
+	List<BoardVO> selectBoardList();
+	
+}
---
+++ Board/src/main/java/com/kh/board/service/BoardServiceImpl.java
@@ -0,0 +1,29 @@
+package com.kh.board.service;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.board.vo.BoardVO;
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService{
+	//spring에서 myBatis를 사용가능하게 하는 객체
+	//SqlSessionTemplate 자료형으로 미리 만들어 둔 객체를 연결시킨다.
+	//@Autowired는 미리 만들어준 객체 중에서 
+	//자료형에 대입될 수 있는 객체를 가져와 객체를 주입한다.
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	//쿼리 실행하는 객체
+	//SqlSessionFactory sqlSessionFactory = SqlSessionManager;
+	//SqlSession sqlSession - sqlSessionFactory.openSession;
+	
+	@Override
+	public List<BoardVO> selectBoardList() {
+		return sqlSession.selectList("boardMapper.selectBoardList");
+	}
+
+}
